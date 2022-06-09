78af5f6385420fd57477cca12fb04bddfebc01aa
LM Heng <89623949+LMActivants@users.noreply.github.com>
2022-06-09 10:54:02 +0800

    Updated API service

---
+++ src/services/core/response.ts
@@ -0,0 +1,7 @@
+export function handleResponse<T>(response: T) {
+  return response;
+}
+
+export function handleError<T>(error: T) {
+  return error;
+}
--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -3,33 +3,34 @@ import * as CryptoJS from "crypto-js";
 const ENCRYPT_KEY: string = process.env.ENCRYPT_KEY!;
 const ENCRYPT_PHRASE: string = process.env.ENCRYPT_PHRASE!;
 
-const encrypt = (input: any): string => {
-  const key = CryptoJS.enc.Utf8.parse(ENCRYPT_KEY);
+class Crypto {
+  // used for encryption of data sent to webservice
+  encrypt(input: any): string {
+    const key = CryptoJS.enc.Utf8.parse(ENCRYPT_KEY);
 
-  const encrypted = CryptoJS.AES.encrypt(
-    CryptoJS.enc.Utf8.parse(JSON.stringify(input)),
-    ENCRYPT_PHRASE,
-    key
-  );
+    const encrypted = CryptoJS.AES.encrypt(
+      CryptoJS.enc.Utf8.parse(JSON.stringify(input)),
+      ENCRYPT_PHRASE,
+      key
+    );
 
-  var e64 = CryptoJS.enc.Base64.parse(encrypted.toString());
-  var eHex = e64.toString(CryptoJS.enc.Hex);
+    var e64 = CryptoJS.enc.Base64.parse(encrypted.toString());
+    var eHex = e64.toString(CryptoJS.enc.Hex);
 
-  return eHex;
-};
+    return eHex;
+  }
 
-const decrypt = (input: string): any => {
-  var key = CryptoJS.enc.Utf8.parse(ENCRYPT_KEY);
+  // used for decryption of data sent from webservice
+  decrypt(input: string): any {
+    var key = CryptoJS.enc.Utf8.parse(ENCRYPT_KEY);
 
-  var reb64 = CryptoJS.enc.Hex.parse(input);
-  var bytes = reb64.toString(CryptoJS.enc.Base64);
+    var reb64 = CryptoJS.enc.Hex.parse(input);
+    var bytes = reb64.toString(CryptoJS.enc.Base64);
 
-  var decrypted = CryptoJS.AES.decrypt(bytes, ENCRYPT_PHRASE, key);
+    var decrypted = CryptoJS.AES.decrypt(bytes, ENCRYPT_PHRASE, key);
 
-  return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
-};
+    return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
+  }
+}
 
-export const Crypto = {
-  encrypt,
-  decrypt,
-};
+export default new Crypto();
